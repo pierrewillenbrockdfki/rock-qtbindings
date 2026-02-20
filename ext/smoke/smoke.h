0d238f6fadeda4f1a3ef63ccb6e09b093b719410
Pierre Willenbrock <pierre.willenbrock@dfki.de>
2026-02-20 11:26:17 +0100

    Expand Smoke::Index to int

--- ext/smoke/smoke.h
+++ ext/smoke/smoke.h
@@ -84,7 +84,7 @@ public:
 	EnumToLong
     };
 
-    typedef short Index;
+    typedef int Index;
     typedef void (*ClassFn)(Index method, void* obj, Stack args);
     typedef void* (*CastFn)(void* obj, Index from, Index to);
     typedef void (*EnumFn)(EnumOperation, Index, void*&, long&);
